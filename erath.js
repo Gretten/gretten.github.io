399f08d2362025356efc5e6a604cf3cd39e22136
julian <someone@proton.me>
2022-12-25 20:39:45 +0500

    +9

--- erath.js
+++ erath.js
@@ -1,25 +1,29 @@
 const findSimpleNumbersInRange = (range) => {
-    if(typeof range !== 'number') {
-        throw new Error('Тип аргумента range должен быть number');
+    try {
+        if(typeof range !== 'number') {
+            throw new Error('Тип аргумента range должен быть number');
+        }
+        if(range < 2) {
+            throw new Error('Диапазон не может быть меньше 2');
+        }
+
+        const complexNumbers = new Map();
+        const simpleNumbers = [];
+
+        for(let i = 2; i <= range; i++) {
+            for(j = i + 1; j <= range; j++) {
+                if(!complexNumbers.get(j) && !(j % i)) {
+                    complexNumbers.set(j, j);
+                } 
+            }
+            if(!complexNumbers.get(i)) {
+                simpleNumbers.push(i);
+            }
+        }
+        return simpleNumbers;
+    } catch(err) {
+        console.log(err.message);
         return null;
     }
-    if(range < 2) {
-        throw new Error('Диапазон не может быть меньше 2');
-        return null;
-    }
-
-    const complexNumbers = new Map();
-    const simpleNumbers = [];
-
-    for(let i = 2; i <= range; i++) {
-        for(j = i + 1; j <= range; j++) {
-            if(!complexNumbers.get(j) && !(j % i)) {
-                complexNumbers.set(j, j);
-            } 
-        }
-        if(!complexNumbers.get(i)) {
-            simpleNumbers.push(i);
-        }
-    }
-    return simpleNumbers;
+        
 }

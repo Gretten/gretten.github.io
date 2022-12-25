521c4ab7230ad71f00f787ba088334c49a8c6c81
julian <someone@proton.me>
2022-12-25 18:15:50 +0500

    +6

--- erath.js
+++ erath.js
@@ -1,4 +1,3 @@
-const a = '2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199'
 const letsFindNonSimples = (range) => {
     if(typeof range !== 'number') {
         return new Error('Тип аргумента range должен быть number');
@@ -17,7 +16,7 @@ const letsFindNonSimples = (range) => {
             } 
         }
         if(!map.get(i)) {
-            result.push(i)
+            result.push(i);
         }
     }
     return result;
